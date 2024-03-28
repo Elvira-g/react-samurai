7aaeba434a97f0d16e53b0d5a614acf078eb9466
elvira <elvira_27@mail.ru>
2024-03-28 17:25:03 +0100

    Updates

---
+++ precache-manifest.e271b71d85622137c3f1cbef60246cf0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a7c00dac5bc94b64ba5e7652049bff",
+    "url": "/react-samurai/index.html"
+  },
+  {
+    "revision": "0354ffa3ed3b89da589d",
+    "url": "/react-samurai/static/css/3.54f219c7.chunk.css"
+  },
+  {
+    "revision": "a5d5dfcc81c7ee445811",
+    "url": "/react-samurai/static/css/4.32249919.chunk.css"
+  },
+  {
+    "revision": "fb2239e7efad232f3215",
+    "url": "/react-samurai/static/css/main.d8765b8a.chunk.css"
+  },
+  {
+    "revision": "82f9dc9835dde6d26098",
+    "url": "/react-samurai/static/js/2.effae9b6.chunk.js"
+  },
+  {
+    "revision": "0354ffa3ed3b89da589d",
+    "url": "/react-samurai/static/js/3.08eac221.chunk.js"
+  },
+  {
+    "revision": "a5d5dfcc81c7ee445811",
+    "url": "/react-samurai/static/js/4.47b52921.chunk.js"
+  },
+  {
+    "revision": "fb2239e7efad232f3215",
+    "url": "/react-samurai/static/js/main.a0259984.chunk.js"
+  },
+  {
+    "revision": "ce1bb02c655750c180c6",
+    "url": "/react-samurai/static/js/runtime~main.7161bd14.js"
+  }
+]);
